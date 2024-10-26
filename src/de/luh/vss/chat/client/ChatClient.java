4033f43f222867bf7436e5c7abcb200b45e19e63
thunderfox456 <thunderfox456@gmail.com>
2024-10-26 16:54:18 +0200

    created first objects

--- src/de/luh/vss/chat/client/ChatClient.java
+++ src/de/luh/vss/chat/client/ChatClient.java
@@ -1,6 +1,12 @@
 package de.luh.vss.chat.client;
 
 import java.io.IOException;
+import java.net.InetAddress;
+
+import de.luh.vss.chat.common.Message.ChatMessage;
+import de.luh.vss.chat.common.Message.RegisterRequest;
+import de.luh.vss.chat.common.User;
+import de.luh.vss.chat.common.User.UserId;
 
 public class ChatClient {
 
@@ -16,6 +22,12 @@ public class ChatClient {
 		System.out.println("Congratulation for successfully setting up your environment for Assignment 1!");
 		
 		// implement your chat client logic here
+		UserId userId = new UserId(9277);
+		
+		User user = new User(userId, null);
+		ChatMessage chatMessage = new ChatMessage(userId, "test");
+		InetAddress inetAddress = InetAddress.getByName("130.75.202.197");
+		RegisterRequest registerRequest = new RegisterRequest(userId, inetAddress, 4444);
 	}
 
 }
