42a7f4fa2a9bae4aa19cb2ca3cfa6ffb037a053b
thunderfox456 <thunderfox456@gmail.com>
2024-10-27 00:06:06 +0200

    added socket

--- src/de/luh/vss/chat/client/ChatClient.java
+++ src/de/luh/vss/chat/client/ChatClient.java
@@ -1,7 +1,10 @@
 package de.luh.vss.chat.client;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 
 import de.luh.vss.chat.common.Message.ChatMessage;
 import de.luh.vss.chat.common.Message.RegisterRequest;
@@ -23,11 +26,14 @@ public class ChatClient {
 		
 		// implement your chat client logic here
 		UserId userId = new UserId(9277);
-		
+		//SocketAddress socketAddress = new SocketAddress();
 		User user = new User(userId, null);
 		ChatMessage chatMessage = new ChatMessage(userId, "test");
 		InetAddress inetAddress = InetAddress.getByName("130.75.202.197");
 		RegisterRequest registerRequest = new RegisterRequest(userId, inetAddress, 4444);
+		Socket socket = new Socket("localhost", 8081);
+		DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+		registerRequest.toStream(dOut);
 	}
 
 }
