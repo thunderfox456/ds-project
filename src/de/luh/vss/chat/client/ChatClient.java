43b306da47313d46095cbf30a1f3abe4e9cba6c6
thunderfox456 <thunderfox456@gmail.com>
2024-10-31 15:51:41 +0100

    passed test 3 and 4

--- src/de/luh/vss/chat/client/ChatClient.java
+++ src/de/luh/vss/chat/client/ChatClient.java
@@ -9,12 +9,15 @@ import java.net.SocketAddress;
 
 import de.luh.vss.chat.common.Message;
 import de.luh.vss.chat.common.Message.ChatMessage;
+import de.luh.vss.chat.common.Message.ErrorResponse;
 import de.luh.vss.chat.common.Message.RegisterRequest;
 import de.luh.vss.chat.common.MessageType;
 import de.luh.vss.chat.common.User;
 import de.luh.vss.chat.common.User.UserId;
 
 public class ChatClient {
+	
+	private static int max_lenght = 4000;
 
 	public static void main(String... args) {
 		try {
@@ -42,9 +45,13 @@ public class ChatClient {
 		registerRequest.toStream(dOut);
 		
 		//sending Message for Test 1 for user ID correctness
-		test1(userId, dOut);
+		//test1(userId, dOut);
 		//sending Message for Test 2 for out of band message
-		test2(userId, dOut, dIn);
+		//test2(userId, dOut, dIn);
+		//sending Message for Test 3 for exceeding max message length
+		//test3(userId, dOut, dIn);
+		//sending Message for Test 4 for handling error message
+		test4(userId, dOut, dIn);
 		
 	}
 	
@@ -89,6 +96,130 @@ public class ChatClient {
 					//chat messages should be echoed
 					if (response.getMessageType().equals(MessageType.CHAT_MESSAGE))
 						response.toStream(dOut);
+				//unknown message type	
+				} catch (IllegalStateException e) { 
+					System.out.println(e);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IllegalStateException e) {
+			System.out.println(e);
+		}
+		
+		return true;
+	}
+	
+	public boolean test3(UserId userId, DataOutputStream dOut, DataInputStream dIn) {
+		System.out.println("Starting Test 3.");
+		//sending Message for Test 3 for exceeding max message length
+		ChatMessage chatMessageTest3 = new ChatMessage(userId, "TEST 3 EXCEEDING MAX MESSAGE LENGTH");
+		
+		try {
+			chatMessageTest3.toStream(dOut);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		Message response;
+		try {
+			//catch all incoming messages
+			while (true) {
+				try {
+					//parse messages incoming from the DataStream
+					response = Message.parse(dIn);
+					
+					//print the message for debug reasons
+					System.out.println(response);
+					//ignore register responses, print for debug reasons
+					if (response.getMessageType().equals(MessageType.REGISTER_RESPONSE))
+						System.out.println(response);
+					//chat messages
+					if (response.getMessageType().equals(MessageType.CHAT_MESSAGE)) {
+						int messageLength = ((ChatMessage)(response)).getMessage().length();
+						System.out.println(messageLength);
+						//the size of the message in bytes, each char is 2 Bytes
+						int byteSize = messageLength * 2;
+						System.out.println(byteSize);
+						//message exceeds max length
+						if (byteSize > max_lenght) {
+							//calculate how much bytes the message is too big
+							int difference = byteSize - max_lenght;
+							System.out.println(difference);
+							//send difference back to server
+							ChatMessage chatMessage = new ChatMessage(userId, String.valueOf(difference));
+							chatMessage.toStream(dOut);
+						}
+						
+					}
+				//unknown message type	
+				} catch (IllegalStateException e) { 
+					System.out.println(e);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IllegalStateException e) {
+			System.out.println(e);
+		}
+		
+		return true;
+	}
+	
+	public boolean test4(UserId userId, DataOutputStream dOut, DataInputStream dIn) {
+		System.out.println("Starting Test 4.");
+		//sending Message for Test 4 for handling error message
+		ChatMessage chatMessageTest4 = new ChatMessage(userId, "TEST 4 HANDLING ERROR MESSAGE");
+		
+		try {
+			chatMessageTest4.toStream(dOut);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		Message response;
+		try {
+			//catch all incoming messages
+			while (true) {
+				try {
+					//parse messages incoming from the DataStream
+					response = Message.parse(dIn);
+					
+					//print the message for debug reasons
+					System.out.println(response);
+					//ignore register responses, print for debug reasons
+					if (response.getMessageType().equals(MessageType.REGISTER_RESPONSE))
+						System.out.println(response);
+					//chat messages
+					if (response.getMessageType().equals(MessageType.CHAT_MESSAGE)) {
+						int messageLength = ((ChatMessage)(response)).getMessage().length();
+						//the size of the message in bytes, each char is 2 Bytes
+						int byteSize = messageLength * 2;
+						//message exceeds max length
+						if (byteSize > max_lenght) {
+							//calculate how much bytes the message is too big
+							int difference = byteSize - max_lenght;
+							System.out.println(difference);
+							//send difference back to server
+							ChatMessage chatMessage = new ChatMessage(userId, String.valueOf(difference));
+							chatMessage.toStream(dOut);
+						}
+						response.toStream(dOut);
+					}
+					if (response.getMessageType().equals(MessageType.ERROR_RESPONSE)) {
+						String errorMessage = ((ErrorResponse)(response)).toString();
+						System.out.println(errorMessage);
+					}
 				//unknown message type	
 				} catch (IllegalStateException e) { 
 					System.out.println(e);
